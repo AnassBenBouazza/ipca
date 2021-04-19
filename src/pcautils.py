cb428705e3eb5f1f7c939294cf61b83fe61459c5
anassb <anassb@sdf-login03.slac.stanford.edu>
2021-04-19 15:42:48 -0700

    Commit 1

---
+++ src/pcautils.py
@@ -0,0 +1,22 @@
+import os
+import sys
+
+import glob
+import h5py
+from matplotlib import pyplot as plt
+from matplotlib.colors import LogNorm
+import skopi as sk
+
+import time
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+from IPython.display import clear_output
+from sklearn.decomposition import PCA, IncrementalPCA
+
+
+class fullPCA :
+    
+    
+class iPCA :
